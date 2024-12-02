5890a07e85d86a2b5628403a15f6f2dac58fd3e3
Gajdboll <krzysztofgajdosz1981@gmail.com>
2024-12-02 19:55:14 +0100

    navigation
    navigation

---
+++ PortalWWW/Controllers/MovieComponents/NavigationComponent.cs
@@ -0,0 +1,20 @@
+﻿using DatabaseAPI.Data;
+using DatabaseAPI.Models.CinemaMovie;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace PortalWWW.Controllers.MovieComponents
+{
+	public class NavigationComponent : ViewComponent
+	{
+		private readonly DatabaseAPIContext dbContext;
+		public NavigationComponent(DatabaseAPIContext context)
+		{
+			dbContext = context;
+		}
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			return View("NavigationComponent");
+		}
+	}
+}
