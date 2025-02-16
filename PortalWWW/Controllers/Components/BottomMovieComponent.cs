f2076f1e0dc397c10ffe370fa549b270596231e8
Gajdboll <krzysztofgajdosz1981@gmail.com>
2025-02-16 22:56:49 +0100

    Auto stash before checking out "origin/master"
    Bottom Component w Kontrolerze

---
+++ PortalWWW/Controllers/Components/BottomMovieComponent.cs
@@ -0,0 +1,18 @@
+﻿using DatabaseAPI.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PortalWWW.Controllers.Components
+{
+    public class BottomMovieComponent : ViewComponent
+    {
+        private readonly DatabaseAPIContext context;
+        public BottomMovieComponent(DatabaseAPIContext context)
+        {
+            this.context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("BottomMovieComponent");
+        }
+    }
+}
